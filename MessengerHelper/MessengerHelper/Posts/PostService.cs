c9b5112388386787ffc058ad6e3af592901e531e
Yuki Sakumoto <cooking.of.the.magic.11@gmail.com>
2018-09-08 15:26:13 +0900

    Add PostService.

---
+++ MessengerHelper/MessengerHelper/Posts/PostService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessengerHelper.Posts
+{
+    class PostService : IPostsService
+    {
+        public IPost<TMessage> CreatePost<TMessage>()
+        {
+            return new ActionPost<TMessage>();
+        }
+    }
+}
